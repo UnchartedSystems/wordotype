14a303971708eb747d7601d23528eebb80e5bc42
Robert812218 <robert812@shaw.ca>
2022-02-13 12:23:40 -0700

    making sure everything is pushed

--- app.js
+++ app.js
@@ -2,18 +2,23 @@ const express = require("express");
 const app = express();
 const PORT = 6969; // default port 8080
 
-// const urlDatabase = {
-//   "b2xVn2": "http://www.lighthouselabs.ca",
-//   "9sm5xK": "http://www.google.com"
-// };
+// const path = require("path");
+// const router = express.Router();
+
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+// app.get("/index.html", (req, res) => {
+//   res.send("yup"); 
+// })
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
-});
+}); 
+
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
