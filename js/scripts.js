3be466b04c6885b05230ef97410b8136c737727e
Robert812218 <robert812@shaw.ca>
2022-02-19 16:26:38 -0700

    not done lol

---
+++ js/scripts.js
@@ -0,0 +1,253 @@
+// const rows = {
+//   r1: {
+//     A: false,
+//     B: false,
+//     C: false,
+//     D: false,
+//     E: false,
+//     F: false,
+//     G: false
+//   },
+
+//   r2: {
+//     A: false,
+//     B: false,
+//     C: false,
+//     D: false,
+//     E: false,
+//     F: false,
+//     G: false
+//   },
+
+//   r3: {
+//     A: false,
+//     B: false,
+//     C: false,
+//     D: false,
+//     E: false,
+//     F: false,
+//     G: false
+//   },
+
+//   r4: {
+//     A: false,
+//     B: false,
+//     C: false,
+//     D: false,
+//     E: false,
+//     F: false,
+//     G: false
+//   },
+
+//   r5: {
+//     A: false,
+//     B: false,
+//     C: false,
+//     D: false,
+//     E: false,
+//     F: false,
+//     G: false
+//   },
+
+//   r6: {
+//     A: false,
+//     B: false,
+//     C: false,
+//     D: false,
+//     E: false,
+//     F: false,
+//     G: false
+//   },
+
+//   r7: {
+//     A: false,
+//     B: false,
+//     C: false,
+//     D: false,
+//     E: false,
+//     F: false,
+//     G: false
+//   }
+// };
+/////////////////////
+const bigObj = {
+
+  rowsObj: {
+      r0: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0,
+        E: 0,
+        F: 0,
+        G: 0
+      },
+    
+      r1: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0,
+        E: 0,
+        F: 0,
+        G: 0
+      },
+    
+      r2: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0,
+        E: 0,
+        F: 0,
+        G: 0
+      },
+    
+      r3: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0,
+        E: 0,
+        F: 0,
+        G: 0
+      },
+    
+      r4: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0,
+        E: 0,
+        F: 0,
+        G: 0
+      },
+    
+      r5: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0,
+        E: 0,
+        F: 0,
+        G: 0
+      },
+    
+      r6: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0,
+        E: 0,
+        F: 0,
+        G: 0
+    }
+  },
+  
+  linksObj: {
+      l05: {
+        A: false,
+        B: false,
+        C: false,
+        D: false,
+        E: false,
+        F: false,
+        G: false
+      },
+    
+      l15: {
+        A: false,
+        B: false,
+        C: false,
+        D: false,
+        E: false,
+        F: false,
+        G: false
+      },
+    
+      l25: {
+        A: false,
+        B: false,
+        C: false,
+        D: false,
+        E: false,
+        F: false,
+        G: false
+      },
+    
+      l35: {
+        A: false,
+        B: false,
+        C: false,
+        D: false,
+        E: false,
+        F: false,
+        G: false
+      },
+    
+      l45: {
+        A: false,
+        B: false,
+        C: false,
+        D: false,
+        E: false,
+        F: false,
+        G: false
+      },
+    
+      l55: {
+        A: false,
+        B: false,
+        C: false,
+        D: false,
+        E: false,
+        F: false,
+        G: false
+    }
+  }
+};
+
+// let tempArr = Object.keys(bigObj[i]);
+// for const i of tempArr
+
+const scanRow = function(targetRow, prevRow, nextRow) {
+  // we scan ze row:
+  // if letter is in row:
+  // 
+  // if (letter && spot) -> tile green
+  // if (letter && !spot) -> tile yellow
+
+  
+
+  
+};
+
+// r0
+  // l0.5: 
+// r1
+  // l1.5
+// r2
+  // l2.5
+// r3
+  // l3.5
+// r4
+  // l4.5
+// r5
+  // l5.5
+// r6
+
+
+
+
+
+
+
+
+
+
+// chicken wings
+// salad rolls
+// spring rolls 
+// 43. pad thai w/ shrimp
+// 25. sweet/sour stir fry
+// 47. pad kee maow (spicy drunken noodle)
